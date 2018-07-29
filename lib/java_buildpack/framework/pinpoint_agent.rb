bda65c3817fee6fe00c3aa65fc71e800e5f5f465
MinSeok Kim <myminseok@gmail.com>
2018-07-29 16:55:15 +0900

    x

--- lib/java_buildpack/framework/pinpoint_agent.rb
+++ lib/java_buildpack/framework/pinpoint_agent.rb
@@ -42,8 +42,8 @@ module JavaBuildpack
         credentials = @application.services.find_service(FILTER)['credentials']
         #credentials = @application.services.find_service(FILTER, "user-provided")['credentials']
 
-        #environment_variables = @droplet.environment_variables
-        #environment_variables.add_environment_variable(PINPOINT_PROFILER_COLLECTOR_IP, credentials[uri])
+        environment_variables = @droplet.environment_variables
+        environment_variables.add_environment_variable(PINPOINT_PROFILER_COLLECTOR_IP, credentials[uri])
 
 
       end
